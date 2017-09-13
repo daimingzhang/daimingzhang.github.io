3c1e06a658c14e433b57e32997fcee3a5f1e0868
daimingzhang <daimingzhang@cca.edu>
2017-09-13 08:51:34 -0700

    d
    d

--- js/bookmarklet_fontup.js
+++ js/bookmarklet_fontup.js
@@ -33,10 +33,10 @@ function initMyBookmarklet(){
         //YOUR CODE GOES HERE!
         var p=document.getElementsByTagName('*');
         for(i=0;i<p.length;i++){
-            if(p[i].style.fontsize){
-                var s=parseInt(p[i].style.fontsize.replace('px',''));
-            }else
-                var s=12;}s+=2;p[i].style.fontsize=s+'px'}
+            if(p[i].style.fontSize){
+                var s=parseInt(p[i].style.fontSize.replace("px",""));
+            } else {
+                var s=12;}s+=2;p[i].style.fontSize=s+"px"}
 
         //YOUR CODE GOES HERE!
     })();
