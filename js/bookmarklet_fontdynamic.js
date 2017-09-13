8aa7c6729c4b9a099bf4b28d834fd04d1a72c777
daimingzhang <daimingzhang@cca.edu>
2017-09-13 09:15:51 -0700

    asdf
    asdf

--- js/bookmarklet_fontdynamic.js
+++ js/bookmarklet_fontdynamic.js
@@ -33,7 +33,7 @@ function initMyBookmarklet(){
         //YOUR CODE GOES HERE!
         $(document).mousemove(function(){
 
-        var p=document.getElementsByTagName('*');
+        var p=document.getElementsByTagName('h1');
         for(i=0;i<p.length;i++){
             if(p[i].style.fontSize){
                 var s=parseInt(p[i].style.fontSize.replace("px",""));
@@ -41,6 +41,27 @@ function initMyBookmarklet(){
                 var s=12;}s+=2;p[i].style.fontSize=s+"px"}
 
             });
+
+        $(document).click(function(){
+
+        var p=document.getElementsByTagName('p');
+        for(i=0;i<p.length;i++){
+            if(p[i].style.fontSize){
+                var s=parseInt(p[i].style.fontSize.replace("px",""));
+            } else {
+                var s=12;}s+=10;p[i].style.fontSize=s+"px"}
+
+
+                var p=document.getElementsByTagName('h1');
+        for(i=0;i<p.length;i++){
+            if(p[i].style.fontSize){
+                var s=parseInt(p[i].style.fontSize.replace("px",""));
+            } else {
+                var s=12;}s+=2;p[i].style.fontSize=s+"px"}
+
+            });
+
+
 
         //YOUR CODE GOES HERE!
     })();
