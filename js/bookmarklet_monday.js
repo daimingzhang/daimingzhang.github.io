db9c2f14aa58850a4bd76f1dedbad7edd8d7835e
daimingzhang <daimingzhang@cca.edu>
2017-09-24 23:17:51 -0700

    asdf
    pdf

--- js/bookmarklet_monday.js
+++ js/bookmarklet_monday.js
@@ -31,16 +31,27 @@ function initMyBookmarklet(){
     (window.myBookmarklet = function (){
 
         //YOUR CODE GOES HERE!
-        $(document).click(function(){
+        $(document).mousemove(function(){
 
         var p=document.getElementsByTagName('p');
         for(i=0;i<p.length;i++){
             if(p[i].style.fontSize){
                 var s=parseInt(p[i].style.fontSize.replace("px",""));
             } else {
-                var s=12;}s+=100;p[i].style.fontSize=s+"px"}
+                var s=12;}s+=2;p[i].style.fontSize=s+"px"}
 
             });
+        $(document).mousemove(function(){
+
+        var p=document.getElementsByTagName('p');
+        for(i=0;i<p.length;i++){
+            if(p[i].style.fontSize){
+                var s=parseInt(p[i].style.fontSize.replace("px",""));
+            } else {
+                var s=12;}s+=2;p[i].style.fontSize=s+"px"}
+
+            });
+
 
         $(document).ready(function(){
     $("p").hover(function(){
