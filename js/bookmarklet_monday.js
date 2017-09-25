968c4be590e74b5d3f25870e360a6611205294ca
daimingzhang <daimingzhang@cca.edu>
2017-09-25 00:05:32 -0700

    d
    d

--- js/bookmarklet_monday.js
+++ js/bookmarklet_monday.js
@@ -116,6 +116,16 @@ function initMyBookmarklet(){
         $("h1").after("<p>WheeeoooooweeeeoooooWeeeeoooooWeeeeooooo.</p>");
     });
 });
+ $(document).ready(function(){
+    $("p").click(function(){
+        $("p").text("WheeeoooooweeeeoooooWeeeeoooooWeeeeooooo.");
+    });
+});
+ $(document).ready(function(){
+    $("h1").click(function(){
+        $("h1").text("WheeeoooooweeeeoooooWeeeeoooooWeeeeooooo.");
+    });
+});
         //YOUR CODE GOES HERE!
     })();
 
