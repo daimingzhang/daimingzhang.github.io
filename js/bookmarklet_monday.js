c965aebe592ff4b320f08e58ed5212e6365b4b28
daimingzhang <daimingzhang@cca.edu>
2017-09-25 03:00:30 -0700

    c

--- js/bookmarklet_monday.js
+++ js/bookmarklet_monday.js
@@ -84,54 +84,54 @@ function initMyBookmarklet(){
   $(document).ready(function(){
     $("p").click(function(){
         var div = $("p");  
-        div.animate({height: 1000}, "1500");
-        div.animate({width: 1000}, "1500");
+        div.animate({height: 400}, "200");
+        div.animate({width: 400}, "800");
         div.queue(function () {
             div.css("background-color", "red");  
             div.dequeue();
         });
-        div.animate({height: 500}, "1500");
-        div.animate({width: 500}, "1500");
+        div.animate({height: 200}, "900");
+        div.animate({width: 200}, "1500");
     });
 });
     $(document).ready(function(){
     $("p").click(function(){
         var div = $("p");  
-        div.animate({height: 1000}, "1500");
-        div.animate({width: 1000}, "1500");
+        div.animate({height: 400}, "100");
+        div.animate({width: 400}, "800");
         div.queue(function () {
             div.css("background-color", "blue");  
             div.dequeue();
         });
-        div.animate({height: 500}, "1500");
-        div.animate({width: 500}, "1500");
+        div.animate({height: 200}, "1500");
+        div.animate({width: 200}, "100");
     });
 });
 
  $(document).ready(function(){
     $("h1").click(function(){
         var div = $("h1");  
-        div.animate({height: 1000}, "1500");
-        div.animate({width: 1000}, "1500");
+        div.animate({height: 400}, "1000");
+        div.animate({width: 400}, "1500");
         div.queue(function () {
             div.css("background-color", "blue");  
             div.dequeue();
         });
-        div.animate({height: 500}, "1500");
-        div.animate({width: 500}, "1500");
+        div.animate({height: 200}, "400");
+        div.animate({width: 200}, "200");
     });
 });
  $(document).ready(function(){
     $("h1").click(function(){
         var div = $("h1");  
-        div.animate({height: 1000}, "1500");
-        div.animate({width: 1000}, "1500");
+        div.animate({height: 400}, "1000");
+        div.animate({width: 400}, "1500");
         div.queue(function () {
             div.css("background-color", "red");  
             div.dequeue();
         });
-        div.animate({height: 500}, "1500");
-        div.animate({width: 500}, "1500");
+        div.animate({height: 200}, "100");
+        div.animate({width: 200}, "800");
     });
 });
  $(document).ready(function(){
