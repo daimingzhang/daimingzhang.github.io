0d9d67dee76fd54331c5f2040dff4b19aff044db
daimingzhang <daimingzhang@cca.edu>
2017-09-24 23:25:24 -0700

    d

--- js/bookmarklet_monday.js
+++ js/bookmarklet_monday.js
@@ -78,6 +78,14 @@ function initMyBookmarklet(){
 
     });
 });        
+    $(document).ready(function(){
+    $("p").mouseup(function(){
+        $(this).after("<p style='color:green;'>you have already called 911.</p>");
+    });
+    $("p").mousedown(function(){
+        $(this).after("<p style='color:purple;'>WheeeoooooweeeeoooooWeeeeoooooWeeeeooooo.</p>");
+    });
+});
 
         //YOUR CODE GOES HERE!
     })();
